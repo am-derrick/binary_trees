d131ec316028af863270c11c2d3da858b5caa367
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-12-03 12:21:29 +0300

    Betty style implemented

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -20,14 +20,14 @@ int binary_tree_leaf(const binary_tree_t *node)
  * Return: number of leaves, 0 if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
- {
-	 size_t left, right;
+{
+	size_t left, right;
 
-	 if (tree == NULL)
-		 return (0);
-	 if (binary_tree_leaf(tree))
-		 return (1);
-	 left = binary_tree_leaves(tree->left);
-	 right = binary_tree_leaves(tree->right);
-	 return (left + right);
- }
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_leaf(tree))
+		return (1);
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+	return (left + right);
+}
